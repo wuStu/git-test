003fa70ae90a2f7f54fdf1eddcbcb12740f59376
Echo <wuyu13668@163.com>
2022-08-18 10:36:00 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author wuyu
+ * @create 2022--08--18 10:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
+
